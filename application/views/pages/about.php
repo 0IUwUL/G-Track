982418a268a7408f2c01386ae1f9d219336eebf3
richard <rasunga30@gmail.com>
2022-05-15 12:39:04 +0800

    feat: Added routes and about page

---
+++ application/views/pages/about.php
@@ -0,0 +1,30 @@
+<div class="container my-5">
+    <div class="title h4">
+        About
+    </div>
+
+    <div class = "status col shadow mb-3 rounded">
+        <div class="justify-content-center mt-3 h6 p-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Molestias aut, repellat ipsum facere voluptate dicta obcaecati
+            deserunt nobis suscipit eaque?
+        </div>
+    </div>
+
+    <div class = "status col shadow mb-5 rounded">
+        <div class="justify-content-center mt-3 h6 p-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Molestias aut, repellat ipsum facere voluptate dicta obcaecati
+            deserunt nobis suscipit eaque?
+        </div>
+    </div>
+
+    <div class = "status col shadow mb-5 rounded">
+        <div class="justify-content-center mt-3 h6 p-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Molestias aut, repellat ipsum facere voluptate dicta obcaecati
+            deserunt nobis suscipit eaque?
+        </div>
+    </div>
+  
+</div>
