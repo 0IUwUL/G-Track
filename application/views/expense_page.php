7053174b0077bc00d6bc88b6e2b6bef8744b81be
0IUwUL <markaljonreales.com>
2022-05-21 12:15:52 +0800

    Expense front end

--- application/views/expense_page.php
+++ application/views/expense_page.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ET App</title>
-    <!--Bootstrap 5 elements link-->
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="style.css">
-    <!-- for calendar -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-</head>
-<body>
     <div class="container mt-5">
         <div class="row">
             <!-- content -->
@@ -64,10 +44,36 @@
                     <div class="days"></div>
                   </div>
             </div>
+        <!-- modal -->
+            <div class="modal fade" id="Inpuexpense" tabindex="-1" aria-labelledby="InputExpense" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content text-color">
+                    <div class="modal-body">
+                      <div class="modal-title justify-content-center h2 text-center">
+                          Expense
+                      </div>
+                      <div class="row mt-5">
+                        <form>
+                            <div class="mb-3">
+                              <label for="InputCostName" class="form-label">Name:</label>
+                              <input type="text" class="form-control" id="InputCostName" aria-describedby="emailHelp">
+                            </div>
+                            <div class="mb-3">
+                              <label for="CostValue" class="form-label">Cost</label>
+                              <input type="number" class="form-control" id="CostValue">
+                            </div>
+                        </form>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
         </div>
     </div>
 
-<script src="script.js"></script>
-</body>
-</html>
+<script src="<?php echo base_url('assets/script/script.js')?>"></script>
 
--- assets/script/script.js
+++ assets/script/script.js
@@ -4,7 +4,8 @@ const renderCalendar = () => {
   date.setDate(1);
 
   const monthDays = document.querySelector(".days");
-
+  const currYear = date.getFullYear();
+  var addDays = 1;
   const lastDay = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
@@ -25,7 +26,7 @@ const renderCalendar = () => {
     0
   ).getDay();
 
-  const nextDays = 7 - lastDayIndex - 1;
+  var nextDays = 0;
 
   const months = [
     "January",
@@ -44,12 +45,12 @@ const renderCalendar = () => {
 
   document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
+  document.querySelector(".date p").innerHTML = currYear;
 
   let days = "";
 
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    days += `<div class="prev-date " role="button" data-bs-toggle="modal" data-bs-target="#Inpuexpense">${prevLastDay - x + 1}</div>`;
   }
 
   for (let i = 1; i <= lastDay; i++) {
@@ -57,16 +58,25 @@ const renderCalendar = () => {
       i === new Date().getDate() &&
       date.getMonth() === new Date().getMonth()
     ) {
-      days += `<div class="today">${i}</div>`;
+      days += `<div class="today btn text-white" role="button" data-bs-toggle="modal" data-bs-target="#Inpuexpense">${i}</div>`;
     } else {
-      days += `<div>${i}</div>`;
+      days += `<div class = "btn text-white" role="button" data-bs-toggle="modal" data-bs-target="#Inpuexpense">${i}</div>`;
     }
+    addDays += 1;
   }
 
+  if (addDays >= 21){
+    nextDays = (7 - lastDayIndex - 1) + 7;
+  }else{
+    nextDays = 7 - lastDayIndex - 1;
+  }
+  console.log(nextDays);
   for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
+    days += `<div class="next-date" role="button" data-bs-toggle="modal" data-bs-target="#Inpuexpense">${j}</div>`;
     monthDays.innerHTML = days;
   }
+  
+
 };
 
 document.querySelector(".prev").addEventListener("click", () => {
@@ -78,5 +88,6 @@ document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
+
 
 renderCalendar();
