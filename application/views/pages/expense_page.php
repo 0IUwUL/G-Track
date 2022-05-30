0446eef31152e163d96191b418cac7c489479e32
0IUwUL <markaljonreales.com>
2022-05-30 21:03:24 +0800

    Expense page finished no backend

--- application/views/pages/expense_page.php
+++ application/views/pages/expense_page.php
@@ -11,13 +11,13 @@
       </div>
     </div>
 
-    <div class="bg_expense p-5 rounded-3 mt-3 position-relative">
+    <div class="bg_expense px-5 rounded-3 mt-3 position-relative">
       <table class="table exp_table">
-        <tr>
-          <th class="text-center">Name</th>
-          <th class="text-center">Date</th>
-          <th class="text-center">Cost</th>
-          <th class = "text-center" colspan="2">Actions</th>
+        <tr class="sticky-top bg_header">
+          <th class="text-center p-3">Name</th>
+          <th class="text-center p-3">Date</th>
+          <th class="text-center p-3">Cost</th>
+          <th class = "text-center p-3" colspan="2">Actions</th>
         </tr>
         <tr class="shadow">
           <td class="text-center p-4"><strong>wow</strong></td>
@@ -33,14 +33,43 @@
           <td class="text-center p-4"><strong><i class="bi bi-pencil-square h5"></i></strong></td>
           <td class="text-center p-4"><strong><i class="bi bi-x-square-fill h5"></i></i></td>
         </tr>
+        <tr class="shadow">
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-pencil-square h5"></i></strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-x-square-fill h5"></i></i></td>
+        </tr>
+        <tr class="shadow">
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-pencil-square h5"></i></strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-x-square-fill h5"></i></i></td>
+        </tr>
+        <tr class="shadow">
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-pencil-square h5"></i></strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-x-square-fill h5"></i></i></td>
+        </tr>
+        <tr class="shadow">
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong>wow</strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-pencil-square h5"></i></strong></td>
+          <td class="text-center p-4"><strong><i class="bi bi-x-square-fill h5"></i></i></td>
+        </tr>
       </table>
-      <div class="row text-light mx-5 position-absolute bottom-0 end-0 pb-4">
-        <div class="text-color d-flex justify-content-end">
-          <strong>Total Expense:</strong>
-        </div>
-        <div class="text-color d-flex justify-content-end">
-          <strong>Budget Remaining:</strong>
-        </div>
+    </div>
+
+    <div class="row text-light pos_status position-absolute bottom-0 end-0 pb-4">
+      <div class="text-color d-flex justify-content-end">
+        <strong>Total Expense: 2130.00</strong>
+      </div>
+      <div class="text-color d-flex justify-content-end">
+        <strong>Budget Remaining: 2130.00</strong>
       </div>
     </div>
   </div>
