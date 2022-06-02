ca1b6b496cf66512f421515cc9af59eaf4497bdf
T0RIEL <je02rebagoda@gmail.com>
2022-06-02 14:01:54 +0800

    Added content in about page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -5,25 +5,28 @@
 
     <div class = "status col shadow mb-3 rounded">
         <div class="justify-content-center mt-3 h6 p-2">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Molestias aut, repellat ipsum facere voluptate dicta obcaecati
-            deserunt nobis suscipit eaque?
+        The purpose of G-Track is to keep track of the user's expenses. 
+        Its purpose is to help its user’s budget and allocate their money.
+        It will help its users to determine their important necessities so that 
+        they prioritize it before their other expenses. 
+        Because not everyone can manage to budget and allocate their money.
         </div>
     </div>
 
     <div class = "status col shadow mb-5 rounded">
         <div class="justify-content-center mt-3 h6 p-2">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Molestias aut, repellat ipsum facere voluptate dicta obcaecati
-            deserunt nobis suscipit eaque?
+        College students can use it to keep track of their expenses.
+        Individuals may become more conscious and cautious about their expenses,
+        and the user will also be able to prioritize their expenses on essentials.
+        Businesses may be able to establish whether or not their business is profitable,
+        as well as manage their employees' salaries and to easily manage their taxes for tax season. 
         </div>
     </div>
 
     <div class = "status col shadow mb-5 rounded">
         <div class="justify-content-center mt-3 h6 p-2">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Molestias aut, repellat ipsum facere voluptate dicta obcaecati
-            deserunt nobis suscipit eaque?
+            The developers behind G-Track consists of Computer Science students from 
+            Technological University of the Philippines.
         </div>
     </div>
   
