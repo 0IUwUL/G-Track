caf61bfd9019bdb3be7873badf95326b5f8b876c
0IUwUL <markaljonreales.com>
2022-06-04 21:15:02 +0800

    Database update for the category

--- database/expense_tracker.sql
+++ database/expense_tracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2022 at 04:37 AM
+-- Generation Time: Jun 04, 2022 at 03:14 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -31,16 +31,9 @@ CREATE TABLE `categories` (
   `id` int(20) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `budget` int(40) NOT NULL
+  `budget` int(40) NOT NULL,
+  `expensed` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `user_id`, `title`, `budget`) VALUES
-(52, 5, 'wow', 123),
-(53, 5, 'hiuo', 100);
 
 -- --------------------------------------------------------
 
@@ -57,14 +50,6 @@ CREATE TABLE `expense` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `expense`
---
-
-INSERT INTO `expense` (`id`, `user_id`, `cat_id`, `name`, `cost`, `date`) VALUES
-(7, 5, 52, 'rawr', 10, '2022-05-06'),
-(9, 5, 53, 'mark', 1, '2022-05-12');
-
 -- --------------------------------------------------------
 
 --
@@ -80,15 +65,6 @@ CREATE TABLE `user` (
   `code` varchar(100) NOT NULL,
   `verified` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `income`, `code`, `verified`) VALUES
-(3, 'richard', 'richardandrei.sunga@tup.edu.ph', '$2y$10$XtiC5oRYzIMXI4VcSI5zguOmVzdbO.STkPtBXAFQHS8uXp39Tz/fW', 0, 'cd97b8405f90', 1),
-(4, 'asd', 'rasunga30@gmail.com', '$2y$10$sp9H3myLNrnUf0wKiqB4wuH2u.RfZcDQtRU8k5kMjq968IyYeqEOq', 0, '4a0b02a4da7f', 1),
-(5, 'Mark', 'markaljonreales@gmail.com', '$2y$10$69TJQE/synK8pviLB9sUPONdK.6kRkSRwkOrzNZOkuuBNFC3Kz4Ry', 0, '2872aebea133', 1);
 
 --
 -- Indexes for dumped tables
@@ -123,19 +99,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `expense`
 --
 ALTER TABLE `expense`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
