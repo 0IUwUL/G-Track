130b7996e7ef722b8ba9617b6da8b66343be39ff
T0RIEL <je02rebagoda@gmail.com>
2022-06-05 22:29:54 +0800

    added minor changes in about page design

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,52 +1,56 @@
 <div class="container pt-5 mt-5">
-    <div class="my-5 d-flex flex-xl-row flex-lg-row flex-md-column flex-column justify-content-center-align-items-center">
+    <div class="col shadow mb-3 rounded">
+        <div class="my-5 d-flex flex-xl-row flex-lg-row flex-md-column flex-column justify-content-center-align-items-center">
 
-        <div class="imglogo">
-            <img src="<?php echo base_url('assets/img/logo.png');?>" class="p-2" alt="imglogo" width="150" height="150">
-        </div>
+            <div class="imglogo">
+                <img src="<?php echo base_url('assets/img/logo.png');?>" class="p-2" alt="imglogo" width="150" height="150">
+            </div>
 
-        <div class="text-#628EFF ps-lg-3 p-3" style="color: #628EFF">
-            <h1 class="card-title">About our Project</h1>
-            <p class="card text"><b>The purpose of G-Track is to keep track of the user's expenses. 
-            Its purpose is to help its user’s budget and allocate their money.
-            It will help its users to determine their important necessities so that 
-            they prioritize it before their other expenses. 
-            Because not everyone can manage to budget and allocate their money.</b></p>
-        </div>
+            <div class="text-#628EFF ps-lg-3 p-3" style="color: #628EFF">
+                <h1 class="card-title">About our Project</h1>
+                <p class="card text"><b>The purpose of G-Track is to keep track of the user's expenses. 
+                Its purpose is to help its user’s budget and allocate their money.
+                It will help its users to determine their important necessities so that 
+                they prioritize it before their other expenses. 
+                Because not everyone can manage to budget and allocate their money.</b></p>
+            </div>
 
-    </div>
-</div>
-
-<div class="container pt-5 mt-5">
-    <div class="my-5 d-flex flex-xl-row flex-lg-row flex-md-column flex-column justify-content-center-align-items-center">
-        
-        <div class="text-#628EFF ps-lg-3 p-3" style="color: #628EFF">
-            <h1 class="card-title">Audience of our Project</h1>
-            <p class="card text"><b>College students can use it to keep track of their expenses.
-            Individuals may become more conscious and cautious about their expenses, 
-            and the user will also be able to prioritize their expenses on essentials. 
-            Businesses may be able to establish whether or not their business is profitable, 
-            as well as manage their employees' salaries and to easily manage their taxes for tax season.</b></p>
-        </div>
-
-        <div class="imgmoney">
-            <img src="<?php echo base_url('assets/img/money.png');?>"class="p-2" alt="imgmoney" width="150" height="150">
         </div>
     </div>
-</div>
 
-<div class="container pt-5 mt-5">
-    <div class="my-5 d-flex flex-xl-row flex-lg-row flex-md-column flex-column justify-content-center-align-items-center">
-        
-        <div class="imgtup">
-            <img src="<?php echo base_url('assets/img/tup.png');?>"class="p-2" alt="imgtup" width="150" height="150">
+    <div class="col shadow mb-3 rounded">
+        <div class="my-5 d-flex flex-xl-row flex-lg-row flex-md-column flex-column justify-content-center-align-items-center">
+            
+            <div class="text-#628EFF ps-lg-3 p-3" style="color: #628EFF">
+                <h1 class="card-title">Audience of our Project</h1>
+                <p class="card text"><b>College students can use it to keep track of their expenses.
+                Individuals may become more conscious and cautious about their expenses, 
+                and the user will also be able to prioritize their expenses on essentials. 
+                Businesses may be able to establish whether or not their business is profitable, 
+                as well as manage their employees' salaries and to easily manage their taxes for tax season.</b></p>
+            </div>
+
+            <div class="imgmoney">
+                <img src="<?php echo base_url('assets/img/money.png');?>"class="p-2" alt="imgmoney" width="150" height="150">
+            </div>
+            
         </div>
+    </div>
 
-        <div class="text-#628EFF ps-lg-3 p-3" style="color: #628EFF">
-            <h1 class="card-title">About the developers</h1>
-            <p class="card text"><b>The developers behind G-Track consists of Computer Science students
-                from Technological University of the Philippines.
-            </b></p>
+    <div class="col shadow mb-3 rounded">
+        <div class="my-5 d-flex flex-xl-row flex-lg-row flex-md-column flex-column justify-content-center-align-items-center">
+            
+            <div class="imgtup">
+                <img src="<?php echo base_url('assets/img/tup.png');?>"class="p-2" alt="imgtup" width="150" height="150">
+            </div>
+
+            <div class="text-#628EFF ps-lg-3 p-3" style="color: #628EFF">
+                <h1 class="card-title">About the developers</h1>
+                <p class="card text"><b>The developers behind G-Track consists of Computer Science students
+                    from Technological University of the Philippines.
+                </b></p>
+            </div>
+
         </div>
     </div>
 </div>
