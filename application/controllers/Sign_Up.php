18e886e48342be237b82aa8e6e59cc94d4b2161a
0IUwUL <markaljonreales.com>
2022-06-11 11:01:07 +0800

    fix: added session for default transaction  on registration

--- application/controllers/Sign_Up.php
+++ application/controllers/Sign_Up.php
@@ -148,11 +148,23 @@ class Sign_Up extends CI_Controller {
                 'set_default' => 1
             );
             $this->transaction->add($data);
+            $val['tran'] = $this->transaction->get($this->session->userdata('user_id'));
+            $val['default'] = $this->deft($val['tran']);
+            $this->session->set_userdata('trans_id', $val['default']['id']);
+
         $this->load->view("pages/verified");
         $this->load->view("template/footer");  
         
         }else {
             echo "Invalid URL";
+        }
+    }
+
+    public function deft($arr){
+        foreach ($arr as $key => $value){
+            if ($value['set_default'] == 1){
+                return $value;
+            }
         }
     }
 }
